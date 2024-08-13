568d8060e9c388f8c09c4569b93c3d0fe8ae9c01
nadiaefimova <efimovanadia85@gmail.com>
2024-08-13 12:02:01 -0400

    Adding Pyspark business seconds function

---
+++ pyspark_function_business_seconds.py
@@ -0,0 +1,33 @@
+from pyspark.sql.functions import unix_timestamp, dayofweek ,datediff, date_trunc, date_add, floor
+
+def business_seconds_between(from_date, to_date):
+    from_date_dow = dayofweek(from_date)
+    to_date_dow = dayofweek(to_date)
+    business_seconds = 0
+    total_diff_days = datediff(to_date, from_date)
+
+    if from_date_dow == 1:
+        new_from_date_unix = unix_timestamp(date_trunc('DD',date_add(from_date,1)))
+        weekend_cal_from_date = date_add(from_date, 1)
+    elif from_date_dow == 7:
+        new_from_date_unix = unix_timestamp(date_trunc('DD',date_add(from_date,2)))
+        weekend_cal_from_date = date_add(from_date, 2)
+    else:
+        new_from_date_unix = unix_timestamp(from_date, format='yyyy-MM-dd HH:mm:ss')
+        weekend_cal_from_date = date_trunc('week',from_date)
+    if to_date_dow == 1:
+        new_to_date_unix = unix_timestamp(date_trunc('DD', date_add(to_date, -1)))
+        weekend_cal_to_date = date_add(to_date, -1)
+    elif to_date_dow == 7:
+        new_to_date_unix = unix_timestamp(date_trunc('DD', date_add(to_date, -2)))
+    else:
+        new_to_date_unix = unix_timestamp(to_date, format='yyyy-MM-dd HH:mm:ss')
+        weekend_cal_to_date = date_trunc('week',to_date)
+
+    total_diff_seconds_cal = new_to_date_unix - new_from_date_unix
+    weekend_days = abs(floor(datediff(weekend_cal_to_date,weekend_cal_from_date)))
+    if total_diff_seconds_cal <= 0 or (to_date_dow in (1,7) and from_date_dow in (1,7) and total_diff_days <= 1):
+        return business_seconds
+    else:
+        business_seconds = total_diff_seconds_cal - (86400 * weekend_days)
+        return business_seconds
